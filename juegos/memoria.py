cbe03c78fb2cb13ee674d17260a31f0ddead8285
benja411 <163329005+benja411@users.noreply.github.com>
2024-03-14 17:38:26 +0000

    subi el juego de memoria.py

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,30 @@
-def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+import random
+import time
+"""n es la cantidad de numeros a memorizar"""
+def memoria(n):
+    list = []
+    n = int(n) 
+    for i in range(n):
+        list += [random.randint(0,9)]
+    return [list]
+print("Cuantos numeros desea memorizar")
+n = input()
+numeros = memoria(n)
+numeros = numeros[0]
+print(numeros)
+time.sleep(5)
+print("\n" * 40)
+print("Ingrese la secuencia de numeros separados por una coma")
+r = input()
+list2 = r.split(",")
+l2 = []
+for i in list2:
+    i = int(i)
+    l2 += [i]
+if numeros == l2:
+    print("Felicidades!, memorizaste correctamente los numeros")
+else:
+    print("Incorrecto! esos no eran los numeros")
+time.sleep(2)
+print('Secuencia original:', numeros)
+print('Tu respuesta:', l2)
