e52036057c5e5f024f799dab930ebdeea99e0fe2
benja411 <163329005+benja411@users.noreply.github.com>
2024-03-14 17:54:22 +0000

    subi memoria.

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,30 +1,27 @@
 import random
 import time
 """n es la cantidad de numeros a memorizar"""
-def memoria(n):
+def memoria():
+    print("Cuantos numeros desea memorizar")
     list = []
-    n = int(n) 
+    n = int(input())
     for i in range(n):
         list += [random.randint(0,9)]
-    return [list]
-print("Cuantos numeros desea memorizar")
-n = input()
-numeros = memoria(n)
-numeros = numeros[0]
-print(numeros)
-time.sleep(5)
-print("\n" * 40)
-print("Ingrese la secuencia de numeros separados por una coma")
-r = input()
-list2 = r.split(",")
-l2 = []
-for i in list2:
-    i = int(i)
-    l2 += [i]
-if numeros == l2:
-    print("Felicidades!, memorizaste correctamente los numeros")
-else:
-    print("Incorrecto! esos no eran los numeros")
-time.sleep(2)
-print('Secuencia original:', numeros)
-print('Tu respuesta:', l2)
+    print(list)
+    time.sleep(5)
+    print("\n" * 40)
+    print("Ingrese la secuencia de numeros separados por una coma")
+    r = input()
+    list2 = r.split(",")
+    l2 = []
+    for i in list2:
+        i = int(i)
+        l2 += [i]
+    if list == l2:
+        print("Felicidades!, memorizaste correctamente los numeros")
+    else:
+        print("Incorrecto! esos no eran los numeros")
+    time.sleep(2)
+    print('Secuencia original:', list)
+    print('Tu respuesta:', l2)
+memoria()
